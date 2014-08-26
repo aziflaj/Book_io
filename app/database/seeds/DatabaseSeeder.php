43f93037d5d688c26ac0bb3982670e33967e25a4
aturhani <arditaturhani@yahoo.com>
2014-08-26 14:16:17 +0200

    added values to table users using faker

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -16,15 +16,27 @@ class DatabaseSeeder extends Seeder {
 
 }
 class UserTableSeeder extends Seeder {
+
     public function run() {
 
+        $faker = Faker\Factory::create();
         DB::table('users')->delete();
         User::create(array(
             'username'      => 'foobar',
             'password'      =>  Hash::make('asdf'),
             'library_name'  => 'first',
-            'email'         => 'firstlibrary@gmail.com',
+            'email'         => 'firstlibrary@gmail.com'
         ));
+
+        for ($i=1; $i < 10; $i++) {
+            User::create(array(
+                'username'      => $faker->userName(),
+                'password'      =>  Hash::make($faker->word()),
+                'library_name'  => $faker->word(),
+                'email'         => $faker->email(),
+            ));
+
+        }
 
     }
 }

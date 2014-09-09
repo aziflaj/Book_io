43fbe37fb8d86c8eeb4ec974fa10afd35c906054
aziflaj <aldoziflaj95@gmail.com>
2014-09-09 13:10:03 -0700

    Auth before accessing /api/v1

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -3,60 +3,44 @@
 class ApiController extends \BaseController {
 
     public function getSearchAuthor($authorname) {
-        if (Auth::check()) {
-            $mybooks = Auth::user()->books;
-            $book_list = array();
-            foreach ($mybooks as $book) {
-                if (! strcmp($book->book->author, $authorname)) {
-                    array_push($book_list,$book->book);
-                }
+        $mybooks = Auth::user()->books;
+        $book_list = array();
+        foreach ($mybooks as $book) {
+            if (! strcmp($book->book->author, $authorname)) {
+                array_push($book_list,$book->book);
             }
-            return Response::json($book_list);
-        } else {
-            return Redirect::to('/login');
         }
+        return Response::json($book_list);
     }
 
     public function getSearchTitle($title) {
-        if (Auth::check()) {
-            $mybooks = Auth::user()->books;
-            $book_list = array();
-            foreach ($mybooks as $book) {
-                if (! strcmp($book->book->title, $title)) {
-                    array_push($book_list,$book->book);
-                }
+        $mybooks = Auth::user()->books;
+        $book_list = array();
+        foreach ($mybooks as $book) {
+            if (! strcmp($book->book->title, $title)) {
+                array_push($book_list,$book->book);
             }
-            return Response::json($book_list);
-        } else {
-            return Redirect::to('/login');
         }
+        return Response::json($book_list);
     }
 
     public function getSearchCategory($category) {
-        if (Auth::check()) {
-            $mybooks = Auth::user()->books;
-            $book_list = array();
-            foreach ($mybooks as $book) {
-                if (! strcmp($book->book->category, $category)) {
-                    array_push($book_list,$book->book);
-                }
+        $mybooks = Auth::user()->books;
+        $book_list = array();
+        foreach ($mybooks as $book) {
+            if (! strcmp($book->book->category, $category)) {
+                array_push($book_list,$book->book);
             }
-            return Response::json($book_list);
-        } else {
-            return Redirect::to('/login');
         }
+        return Response::json($book_list);
     }
 
     public function getBookList() {
-        if (Auth::check()) {
-            $mybooks = Auth::user()->books;
-            $book_list = array();
-            foreach ($mybooks as $book) {
-                    array_push($book_list,$book->book);
-            }
-            return Response::json($book_list);
-        } else {
-            return Redirect::to('/login');
+        $mybooks = Auth::user()->books;
+        $book_list = array();
+        foreach ($mybooks as $book) {
+            array_push($book_list,$book->book);
         }
+        return Response::json($book_list);
     }
 }
--- app/routes.php
+++ app/routes.php
@@ -1,27 +1,6 @@
 <?php
 
-Route::get('/hello', function() {
-    return Response::json(array(
-        'message'   => 'Hello, World!'
-    ));
-});
-
-Route::get('/user/{id}', function($id) {
-
-    $books = array();
-
-    foreach(User::find($id)->books as $book) {
-        array_push($books, $book->book);
-    }
-
-    return Response::json(array(
-        'id'        => $id,
-        'user'      => User::find($id),
-        'books'     => $books
-    ));
-});
-
-Route::group(array('prefix' => 'api/v1'), function() {
+Route::group(array('prefix' => 'api/v1','before' => 'auth'), function() {
 
     Route::controller('/','ApiController');
 });
